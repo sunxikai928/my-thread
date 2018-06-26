16acd4e8f45ce0cee0b2c3673de04269ab29c140
sunxikai <xikai.sun@pactera.com>
2018-06-27 07:23:04 +0800

    搭架子

---
+++ src/main/java/sunxikai928/com/github/example/Main.java
@@ -0,0 +1,15 @@
+package sunxikai928.com.github.example;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by sunxikai on 18/6/27.
+ */
+public class Main {
+    private static Logger logger = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        logger.info("服务器启动了");
+    }
+}
